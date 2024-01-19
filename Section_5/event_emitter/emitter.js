8806428bae239bcec910e8e125d8fa412ae14a6a
sixLUKEY <marcoslukerojo@gmail.com>
2024-01-19 15:05:55 +0200

    event emitter part 1 complete

---
+++ Section_5/event_emitter/emitter.js
@@ -0,0 +1,19 @@
+class Emitter {
+    constructor() {
+        this.events = {};
+    }
+}
+
+Emitter.prototype.on = function(type, listener){
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+}
+
+Emitter.prototype.emit = function(type){
+    if (this.events[type]){
+        this.events[type].forEach((listener) => {
+            listener()
+        });
+    }
+}
+module.exports = Emitter;
