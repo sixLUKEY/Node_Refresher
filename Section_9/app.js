b5d703342f9f77e0de78fb054be28ba1bff44af4
sixLUKEY <marcoslukerojo@gmail.com>
2024-01-30 13:13:20 +0200

    querystring and post parameters complete

--- Section_9/app.js
+++ Section_9/app.js
@@ -1,7 +1,11 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 let port = process.env.PORT || 3000;
+
+let urlEncodedParser = bodyParser.urlencoded({ extended: false });
+let jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -14,14 +18,26 @@ app.use('/', (req, res, next) => {
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
 app.get('/api', (req, res) => {
     res.json({ firstName: 'John', lastName: 'Evans' })
-})
+});
 
 app.get('/person/:id', (req, res) => {
-    res.render('person', { id: req.params.id })
-})
+    res.render('person', { id: req.params.id, qstr: req.query.qstr })
+});
+
+app.post('/person', urlEncodedParser, (req, res) => {
+    res.send('Thank You');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+});
+
+app.post('/personjson', jsonParser, ( req, res ) => {
+    res.send('Thank you for the json data');
+    console,log(req.body.firstName);
+    console,log(req.body.lastName);
+});
 
 app.listen(port);
